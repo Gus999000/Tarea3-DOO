fb2febc66bfec222818ab411e301b7dced5894a2
gustavo <gustavo.gonzalez8190@gmail.com>
2024-05-26 00:52:34 -0400

    Creado el panel de comprador (no es funcional).

---
+++ src/main/java/parte_gráfica/PanelComprador.java
@@ -0,0 +1,7 @@
+package parte_gráfica;
+/**Panel de la clase comprador
+ * @author Gustavo González
+ * @version versión 1, 26 de mayo 2024*/
+import javax.swing.JPanel;
+public class PanelComprador extends JPanel {
+}
