1d63dc22407102196426a8e5ab1e738d24c4965e
gustavo <gustavo.gonzalez8190@gmail.com>
2024-06-02 19:23:29 -0400

    Esqueleto, la idea es un panel que dibuje un circulo de distinto color segun la moneda. La idea es que sea puesto en los depositos correspondientes.

--- src/main/java/parte_gráfica/PanelMoneda.java
+++ src/main/java/parte_gráfica/PanelMoneda.java
@@ -1,5 +1,17 @@
 package parte_gráfica;
-/***/
+/**Panel de la clase moneda
+ * @author Gustavo González
+ * @version versión 1, 2 de junio de 2024*/
+import parte_lógica.Moneda;
 import javax.swing.JPanel;
+import java.awt.*;
 public class PanelMoneda extends JPanel {
+    /***/
+    private Moneda M;
+    /***/
+    public PanelMoneda(Moneda M) {
+    }
+    @Override
+    public void paintComponent(Graphics g) {
+    }
 }
