a9b1b26742314cb3a4e73c3748a1cd733b751600
gustavo <gustavo.gonzalez8190@gmail.com>
2024-06-02 19:25:50 -0400

    Esqueleto, la idea es que ponga una imagen del producto correspondiente.

--- src/main/java/parte_gráfica/PanelProducto.java
+++ src/main/java/parte_gráfica/PanelProducto.java
@@ -1,8 +1,11 @@
 package parte_gráfica;
-/***/
+/**Panel de los productos
+ * @author Gustavo González
+ * @version 1, 2 de junio de 2024*/
+import parte_lógica.Producto;
 import javax.swing.JPanel;
 public class PanelProducto extends JPanel {
-    public PanelProducto() {
+    public PanelProducto(Producto P) {
         //if() {}
     }
 }
