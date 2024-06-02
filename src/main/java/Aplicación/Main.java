6003446f03597819d4b8e0f8e958c1faf13688b1
gustavo <gustavo.gonzalez8190@gmail.com>
2024-06-02 00:23:24 -0400

    Instancia de ventana mas "correcta".

--- src/main/java/Aplicación/Main.java
+++ src/main/java/Aplicación/Main.java
@@ -2,6 +2,6 @@ package Aplicación;
 import parte_gráfica.Ventana;
 public class Main {
     public static void main(String[] args) {
-        Ventana V = new Ventana();
+        new Ventana();
     }
 }
