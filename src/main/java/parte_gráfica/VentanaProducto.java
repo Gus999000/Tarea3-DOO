6ec82f964ff06255b64e56d97ac59bdb802bb854
gustavo <gustavo.gonzalez8190@gmail.com>
2024-06-02 19:28:06 -0400

    Ventana del producto.

---
+++ src/main/java/parte_gráfica/VentanaProducto.java
@@ -0,0 +1,11 @@
+package parte_gráfica;
+/***/
+import javax.swing.*;
+public class VentanaProducto extends JFrame {
+    public VentanaProducto() {
+        this.setSize(500, 400);
+        this.setTitle("Producto");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+}
