05c3cb883ca4acd1ac0a0e8e929f427900fd48a7
gustavo <gustavo.gonzalez8190@gmail.com>
2024-06-02 19:11:53 -0400

    Cambio de nombre.

--- src/main/java/parte_gráfica/VentanaAplicación.java
+++ src/main/java/parte_gráfica/VentanaAplicación.java
@@ -1,12 +1,12 @@
 package parte_gráfica;
 /**Ventana de la aplicación
  * @author Gustavo González
- * @version versión 3, 2 de junio de 2024*/
+ * @version versión 4, 2 de junio de 2024*/
 import javax.swing.JFrame;
 import java.awt.BorderLayout;
-public class Ventana extends JFrame {
+public class VentanaAplicación extends JFrame {
     /**Constructor de la ventana*/
-    public Ventana() {
+    public VentanaAplicación() {
         super();
         this.setSize(1500, 800);
         this.setTitle("Aplicación");
