d17d7bc3f8362f2dc735a5fd3ac1e6f979347add
gustavo <gustavo.gonzalez8190@gmail.com>
2024-06-02 19:13:11 -0400

    Cambio de nombre.

--- src/main/java/Aplicación/Main.java
+++ src/main/java/Aplicación/Main.java
@@ -1,7 +1,7 @@
 package Aplicación;
-import parte_gráfica.Ventana;
+import parte_gráfica.VentanaAplicación;
 public class Main {
     public static void main(String[] args) {
-        new Ventana();
+        new VentanaAplicación();
     }
 }
