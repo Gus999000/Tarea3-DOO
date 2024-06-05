a2144a66dbb2bf79ecc1740e038469ecaf3fcc65
gustavo <gustavo.gonzalez8190@gmail.com>
2024-06-05 17:05:16 -0400

    PanelProducto listo y funcional.

--- src/main/java/parte_gráfica/PanelProducto.java
+++ src/main/java/parte_gráfica/PanelProducto.java
@@ -5,14 +5,40 @@ package parte_gráfica;
 import parte_lógica.*;
 import javax.swing.*;
 import java.awt.*;
+import java.util.Objects;
+
 public class PanelProducto extends JPanel {
     /**Producto recibido por el panel*/
     private Producto P;
     /**Constructor del panel de productos*/
     public PanelProducto(Producto P) {
         this.P = P;
+        this.setSize(100, 100);
+        JLabel NSerie = new JLabel("5");
+        this.add(NSerie);
     }
     @Override
     public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if(Objects.equals(P.getNombre(), "CocaCola")) {
+            ImageIcon PIcon = new ImageIcon(getClass().getClassLoader().getResource("Pcocacola.png"));
+            g.drawImage(PIcon.getImage(), 0, 0, null);
+        }
+        else if(Objects.equals(P.getNombre(), "Sprite")) {
+            ImageIcon PIcon = new ImageIcon(getClass().getClassLoader().getResource("Psprite.png"));
+            g.drawImage(PIcon.getImage(), 0, 0, null);
+        }
+        else if(Objects.equals(P.getNombre(), "Fanta")) {
+            ImageIcon PIcon = new ImageIcon(getClass().getClassLoader().getResource("Pfanta.png"));
+            g.drawImage(PIcon.getImage(), 0, 0, null);
+        }
+        else if(Objects.equals(P.getNombre(), "Snickers")) {
+            ImageIcon PIcon = new ImageIcon(getClass().getClassLoader().getResource("Psnickers.png"));
+            g.drawImage(PIcon.getImage(), 0, 0, null);
+        }
+        else if(Objects.equals(P.getNombre(), "Super8")) {
+            ImageIcon PIcon = new ImageIcon(getClass().getClassLoader().getResource("Psuper8.png"));
+            g.drawImage(PIcon.getImage(), 0, 0, null);
+        }
     }
 }
